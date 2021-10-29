a0c2f19b001479146c3e12918b9e8bade20c0057
Maksim Zotikov <71289146+MaxZotik@users.noreply.github.com>
2021-10-30 00:34:08 +0300

    Edited file Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace Homework_Theme_01
 {
     class Program
     {
+        /// <summary>
+        /// Главный метод. Точка запуска программы.
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
         static void Main(string[] args)
         {
+
+            #region Задание
             // Заказчик просит написать программу «Записная книжка». Оплата фиксированная - $ 120.
 
             // В данной программе, должна быть возможность изменения значений нескольких переменных для того,
@@ -35,9 +41,76 @@ namespace Homework_Theme_01
             // **
             // 5. В качестве бонусной части, за дополнительную оплату $50, заказчик просит реализовать 
             //    возможность вывода данных в центре консоли.
+            #endregion
 
-            
+            #region Задача - 1
 
-        }
+            string name;        //Переменная имя
+            int age;            //Переменная возраст
+            int height;         //Переменная рост
+            double history;     //Переменная балл по истории
+            double mathematics; //Переменная балл по математике
+            double russian;     //Переменная балл по русскому языку
+            double average;     //Переменная средний балл по предметам
+
+            //Ввод данных
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите рост: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите бал по истории: ");
+            history = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите бал по математике: ");
+            mathematics = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите бал по русскому языку: ");
+            russian = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            #endregion
+
+            #region Задача - 2
+
+            //Подсчет среднего балла по предметам
+            average = (history + mathematics + russian) / 3;
+            Console.ReadKey();
+
+            #endregion
+
+            #region Задача - 3 и 5
+
+            //Обычный вывод
+            string pattern1 = name + " " + age + " " + height + " " + average.ToString("#.#");
+            Console.SetCursorPosition((Console.WindowWidth - pattern1.Length) / 2, Console.CursorTop); //Вывод данных в центре консоли
+            Console.WriteLine(pattern1);
+            Console.ReadKey();
+            Console.Clear();
+
+            //форматированный вывод
+            string pattern2 = "Имя: {0} Возраст: {1} Рост: {2} Средний бал по предметам: {3}";
+            Console.SetCursorPosition((Console.WindowWidth - pattern2.Length) / 2, Console.CursorTop); //Вывод данных в центре консоли
+            Console.WriteLine(pattern2,
+                              name,
+                              age,
+                              height,
+                              average.ToString("#.#"));
+            Console.ReadKey();
+            Console.Clear();
+
+            //Использование интерполяции строк
+            string pattern3 = $"Имя: {name} Возраст: {age} Рост: {height} Средний бал по предметам: {average.ToString("#.#")}";
+            Console.SetCursorPosition((Console.WindowWidth - pattern3.Length) / 2, Console.CursorTop); //Вывод данных в центре консоли
+            Console.WriteLine(pattern3);
+            Console.ReadKey();
+
+            #endregion
+       }
     }
 }
